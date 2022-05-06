e7f541dcbd3127e289a00054aa5935d866b49bc4
Baoqi <qi@cnyunguo.com>
2022-05-07 07:49:57 +0800

    first

---
+++ src/renderer/components/LandingPage.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="wrapper">
+   
+  </div>
+</template>
+
+<script>
+  import SystemInformation from './LandingPage/SystemInformation'
+
+  export default {
+    name: 'landing-page',
+    components: { SystemInformation },
+    methods: {
+      open (link) {
+        this.$electron.shell.openExternal(link)
+      }
+    }
+  }
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+    padding: 0;
+  }
+
+  body { font-family: 'Source Sans Pro', sans-serif; }
+
+  #wrapper {
+    background:white;
+    height: 100vh;
+    padding: 60px 80px;
+    width: 100vw;
+  }
+
+</style>
